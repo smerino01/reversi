bd5388d37eb5730c9be0424fa4310e2abb3d170a
Skylar <skylar.lynner@gmail.com>
2021-11-28 19:31:17 -0600

    add servlet file

---
+++ P5_Team_SMSL_Reversi/src/main/java/edu/metrostate/ics425/reversi/team_smsl/controller/PlayGameServlet.java
@@ -0,0 +1,44 @@
+package edu.metrostate.ics425.reversi.team_smsl.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PlayGameServlet
+ */
+@WebServlet("/moveDisk")
+public class PlayGameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * Constructor
+     */
+    public PlayGameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) {
+		// get
+		
+	}
+
+}
